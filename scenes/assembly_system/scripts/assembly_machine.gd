de9ca13a6e5fa52521e948db0efe0b6dc0aae558
DoomFishy <calvinlu350@gmail.com>
2025-10-13 14:30:05 -0400

    work work

---
+++ scenes/assembly_system/scripts/assembly_machine.gd
@@ -0,0 +1,23 @@
+extends Node3D
+
+@export var packagedBurger : PackedScene
+var ingredientArray : Array
+
+func add_ingredient(s):
+	ingredientArray.append(s)
+
+func _on_item_area_entered(area: Area3D) -> void:
+	if area.is_in_group("ingredient"):
+		add_ingredient(area)
+
+func spawn_packaged_burger():
+	$TempLabel.text = ""
+	var instance = packagedBurger.instantiate()
+	instance.pass_ingredients(ingredientArray)
+	add_child(instance)
+	instance.position = $BurgerSpawn.position
+	
+	for i in ingredientArray.size():
+		$TempLabel.text += ingredientArray[i].ingredientName + "\n"
+		ingredientArray[i].queue_free()
+	ingredientArray.clear()
